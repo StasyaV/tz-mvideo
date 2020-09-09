c96c63a27862197643d036159e22793d0f4544a5
StasyaV <stasya522@mail.ru>
2020-09-09 15:45:57 +0300

    Стилизация мобильной версии. Оживление меню - скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
+  let menuButton = document.querySelector(".menu-button");
+  let topNavLocal = document.querySelector(".top-nav--local");
+  let topNavInfo = document.querySelector(".top-nav--info");
+  let mainNav = document.querySelector(".main-nav");
+  let userNav = document.querySelector(".user-nav__account");
+  let closeClass ="close";
+  let openClass = "menu-button--open";
   
+  menuButton.addEventListener("click", () => {
+    topNavLocal.classList.toggle(closeClass);
+    topNavInfo.classList.toggle(closeClass);
+    mainNav.classList.toggle(closeClass);
+    userNav.classList.toggle(closeClass);
+    menuButton.classList.toggle(openClass);
+  });
 });
+
